01e0f6b008ecce8b261c9f2b5906efc7c6f75a51
Mark <mazhenjin@sina.com>
2015-10-13 03:56:07 -0700

    build UI

--- app/src/androidTest/java/com/icomp/isscp/ApplicationTest.java
+++ app/src/androidTest/java/com/icomp/isscp/ApplicationTest.java
@@ -6,9 +6,7 @@ import android.test.ApplicationTestCase;
 import com.icomp.isscp.resp.RespLogin;
 import com.mark.mobile.volley.RespListenerToast;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
