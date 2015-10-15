d47f446ce52b93c8dea287dcadaa572098fec47a
mazhenjin <mazhenjin@sina.com>
2015-10-15 17:17:57 +0800

    remove fullscreen;  fullscreen mode doesn't resize

--- app/src/main/java/com/icomp/isscp/LoginActivity.java
+++ app/src/main/java/com/icomp/isscp/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_login);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
 
         findViewById(R.id.text_reg).setOnClickListener(new View.OnClickListener() {
