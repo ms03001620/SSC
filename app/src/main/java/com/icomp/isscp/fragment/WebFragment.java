5f6b544740904a17715737044106420e6d207c67
mazhenjin <mazhenjin@sina.com>
2015-10-15 16:38:33 +0800

    add webview

---
+++ app/src/main/java/com/icomp/isscp/fragment/WebFragment.java
@@ -0,0 +1,147 @@
+package com.icomp.isscp.fragment;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.icomp.isscp.R;
+import com.icomp.isscp.resp.RespLogin;
+import com.mark.mobile.utils.LogUtils;
+
+
+public class WebFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private RespLogin mParam2;
+    private OnFragmentInteractionListener mListener;
+    private WebView webview;
+    private ProgressBar progressBar;
+    NotificationManager manager;
+
+    public static WebFragment newInstance(String param1, RespLogin param2) {
+        WebFragment fragment = new WebFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putParcelable(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WebFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getParcelable(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
+        manager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        webview = (WebView)view.findViewById(R.id.webview);
+        progressBar = (ProgressBar)view.findViewById(R.id.progressbar);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+                //showNotifyLoading();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+                //dismissNotifyLoading();
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            }
+        });
+
+        LogUtils.paintD("loadUrl:",mParam1);
+        webview.loadUrl(mParam1);
+        return view;
+    }
+
+    public boolean onBackPressed(){
+        if(webview.canGoBack()){
+            webview.goBack();
+            return true;
+        }
+        return false;
+    }
+
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    public void showNotifyLoading(){
+        Notification myNotify = new Notification();
+        myNotify.icon = R.drawable.icon_tel;
+        myNotify.tickerText = "加载中";
+        myNotify.when = System.currentTimeMillis();
+        myNotify.flags = Notification.FLAG_NO_CLEAR;// 不能够自动清除
+   /*      RemoteViews rv = new RemoteViews(getPackageName(), R.layout.my_notification);
+       rv.setTextViewText(R.id.text_content, "hello wrold!");
+        myNotify.contentView = rv;*/
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 1, intent, 1);
+        myNotify.contentIntent = contentIntent;
+        manager.notify(100159, myNotify);
+
+    }
+
+    public void dismissNotifyLoading() {
+        manager.cancel(100159);
+    }
+
+}
