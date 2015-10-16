d2d391d286ca26743d911464d2655a5fa5fb2329
mazhenjin <mazhenjin@sina.com>
2015-10-16 14:51:00 +0800

    test forgetpwd API

--- app/src/main/java/com/icomp/isscp/PwdForgetActivity.java
+++ app/src/main/java/com/icomp/isscp/PwdForgetActivity.java
@@ -1,9 +1,21 @@
 package com.icomp.isscp;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.icomp.isscp.resp.RespLogin;
+import com.mark.mobile.volley.RespListenerDialogToast;
 
 public class PwdForgetActivity extends BaseActivity {
+
+    private EditText mEditMail;
+    private EditText mEditId;
+    private EditText mEditName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,29 @@ public class PwdForgetActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mEditMail = (EditText)findViewById(R.id.edit_email);
+        mEditId = (EditText)findViewById(R.id.edit_id);
+        mEditName = (EditText)findViewById(R.id.edit_name);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                if (TextUtils.isEmpty(mEditMail.getText()) || TextUtils.isEmpty(mEditId.getText())|| TextUtils.isEmpty(mEditName.getText())) {
+                    Toast.makeText(PwdForgetActivity.this, "请填写完整信息", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+               NetTaskContext.getInstance().findPwd(mEditMail.getText().toString(), mEditId.getText().toString(), mEditId.getText().toString(),new RespListenerDialogToast<RespLogin>(PwdForgetActivity.this) {
+                   @Override
+                   public void onResponse(RespLogin resp) {
+                       if (!resp.isError()) {
+                           finish();
+                       }
+                       Snackbar.make(v, resp.getReMsg(), Snackbar.LENGTH_LONG).show();
+                   }
+               });
+            }
+        });
     }
 }
