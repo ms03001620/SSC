08623c7321c2a369afabe59911b47a005818f85a
mazhenjin <mazhenjin@sina.com>
2015-10-16 15:13:45 +0800

    add email vaild

--- markcore/src/main/java/com/mark/mobile/utils/StringUtils.java
+++ markcore/src/main/java/com/mark/mobile/utils/StringUtils.java
@@ -645,4 +645,23 @@ public class StringUtils {
 
     }
 
+    /**
+     * method is used for checking valid email id format.
+     *
+     * @param email
+     * @return boolean true for valid false for invalid
+     */
+    public static boolean isEmail(String email) {
+        boolean isValid = false;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (matcher.matches()) {
+            isValid = true;
+        }
+        return isValid;
+    }
 }
