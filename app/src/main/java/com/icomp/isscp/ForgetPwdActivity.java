712488de9befdc18f3bd782c5910f150729d6e4c
mazhenjin <mazhenjin@sina.com>
2015-10-16 10:24:08 +0800

    add forget pwd activity

---
+++ app/src/main/java/com/icomp/isscp/ForgetPwdActivity.java
@@ -0,0 +1,13 @@
+package com.icomp.isscp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgetPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_pwd);
+    }
+}
