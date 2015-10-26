42618a97c85f4db92c808925632425e0af6542b6
mazhenjin <mazhenjin@sina.com>
2015-10-26 17:36:47 +0800

    add progressDialog theme

--- markcore/src/main/java/com/mark/mobile/volley/RespListenerDialogToast.java
+++ markcore/src/main/java/com/mark/mobile/volley/RespListenerDialogToast.java
@@ -7,15 +7,28 @@ import android.content.Context;
 public abstract class RespListenerDialogToast<T> extends RespListenerToast<T> {
     private ProgressDialog mDialog;
 
-    public RespListenerDialogToast(Context context, String message){
-        mDialog = new ProgressDialog(context);
+    public RespListenerDialogToast(Context context, String message, int theme){
+        if(theme!=0){
+            mDialog = new ProgressDialog(context, theme);
+        }else{
+            mDialog = new ProgressDialog(context);
+        }
+
         mDialog.setMessage(message);
         mDialog.setCancelable(true);
         mDialog.setCanceledOnTouchOutside(false);
     }
 
+    public RespListenerDialogToast(Context context, String message){
+        this(context, message, 0);
+    }
+
+    public RespListenerDialogToast(Context context, int theme){
+        this(context, "正在提交服务器，请稍后...", theme);
+    }
+
     public RespListenerDialogToast(Context context){
-        this(context, "正在提交服务器，请稍后...");
+        this(context, "正在提交服务器，请稍后...", 0);
     }
 
     public void onShow() {
