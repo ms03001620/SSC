890fb4470eb433d6b86cfb04ef6d9eddde148df8
Mark <mazhenjin@sina.com>
2015-10-29 20:39:34 -0700

    webview get html

--- app/src/main/java/com/icomp/isscp/fragment/WebFragment.java
+++ app/src/main/java/com/icomp/isscp/fragment/WebFragment.java
@@ -1,13 +1,8 @@
 package com.icomp.isscp.fragment;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -16,15 +11,14 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 
 import com.icomp.isscp.R;
 import com.icomp.isscp.SettingActivity;
 import com.icomp.isscp.resp.RespLogin;
 import com.mark.mobile.utils.LogUtils;
-
 
 public class WebFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -34,8 +28,9 @@ public class WebFragment extends Fragment {
     private RespLogin mUser;
     private OnFragmentInteractionListener mListener;
     private WebView webview;
-    private ProgressBar progressBar;
-    NotificationManager manager;
+
+    public WebFragment() {
+    }
 
     public static WebFragment newInstance(String param1, RespLogin param2) {
         WebFragment fragment = new WebFragment();
@@ -44,9 +39,6 @@ public class WebFragment extends Fragment {
         args.putParcelable(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public WebFragment() {
     }
 
     @Override
@@ -77,57 +69,53 @@ public class WebFragment extends Fragment {
             }
         });
 
-        if(mUrl.endsWith("My/Index")){
-            view.findViewById(R.id.appbarlayout).setVisibility(View.VISIBLE);
-        }else{
-            view.findViewById(R.id.appbarlayout).setVisibility(View.GONE);
+        if (mUrl.endsWith("My/Index")) {
+            toolbar.setVisibility(View.VISIBLE);
+        } else {
+            toolbar.setVisibility(View.GONE);
         }
 
-        manager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-        webview = (WebView)view.findViewById(R.id.webview);
-        progressBar = (ProgressBar)view.findViewById(R.id.progressbar);
+        webview = (WebView) view.findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
+        //webview.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+
         webview.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                LogUtils.paintD("shouldOverrideUrlLoading:", url);
+                onButtonPressed(url);
                 view.loadUrl(url);
                 return true;
-                //http://stackoverflow.com/questions/5116909/how-i-can-get-onclick-event-on-webview-in-android
             }
 
             public void onPageFinished(WebView view, String url) {
-                progressBar.setVisibility(View.GONE);
-                //showNotifyLoading();
+                //webview.loadUrl("javascript:window.HTMLOUT.processHTML(document.getElementsByTagName('html')[0].innerHTML);");
             }
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                progressBar.setVisibility(View.VISIBLE);
-                //dismissNotifyLoading();
             }
 
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
             }
         });
 
-        String realUrl = makeRealUrl(mUrl, mUser);
-        LogUtils.paintD("loadUrl:", realUrl);
-        if(!TextUtils.isEmpty(mUrl)){
-            webview.loadUrl(realUrl);
+        LogUtils.paintD("loadUrl:", mUrl);
+        if (!TextUtils.isEmpty(mUrl)) {
+            webview.loadUrl(mUrl);
         }
         return view;
     }
 
-    public boolean onBackPressed(){
-/*        if(webview.canGoBack()){
-            webview.goBack();
-            return true;
-        }*/
-        return false;
+    class MyJavaScriptInterface {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processHTML(String html){
+            LogUtils.paintD("processHTML:", html);
+        }
     }
 
 
-    public void onButtonPressed(Uri uri) {
+    public void onButtonPressed(String uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
@@ -151,39 +139,6 @@ public class WebFragment extends Fragment {
     }
 
     public interface OnFragmentInteractionListener {
-        public void onFragmentInteraction(Uri uri);
-    }
-
-    public void showNotifyLoading(){
-        Notification myNotify = new Notification();
-        myNotify.icon = R.drawable.icon_tel;
-        myNotify.tickerText = "加载中";
-        myNotify.when = System.currentTimeMillis();
-        myNotify.flags = Notification.FLAG_NO_CLEAR;// 不能够自动清除
-   /*      RemoteViews rv = new RemoteViews(getPackageName(), R.layout.my_notification);
-       rv.setTextViewText(R.id.text_content, "hello wrold!");
-        myNotify.contentView = rv;*/
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 1, intent, 1);
-        myNotify.contentIntent = contentIntent;
-        manager.notify(100159, myNotify);
-
-    }
-
-    public void dismissNotifyLoading() {
-        manager.cancel(100159);
-    }
-
-
-    public String makeRealUrl(String url, RespLogin user){
-        if(!"http://dldx.mob.sigilsoft.com/UserService/TokenLogin?TokenID=".equals(url)){
-            return url;
-        }
-
-
-        StringBuffer sb = new StringBuffer();
-        sb.append(url);
-        sb.append(user.getReData());
-        return sb.toString();
+        void onFragmentInteraction(String uri);
     }
 }
